49017b18942ee3ae933534d40141b94cf6757a2b
ericchuhong <ericchuhong@gmail.com>
2016-03-03 00:00:16 +0800

    ［功能］新增基于底层封装的web中端

---
+++ www/coroweb.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Chuhong Ma'
+
+import asyncio, os, inspect, logging, functools
+
+from urllib import parse
+
+from aiohttp import web
